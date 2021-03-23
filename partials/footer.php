02f0250e2d515bb4838c15dc40f6378edd9f8df3
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-03-23 21:09:05 +0200

    remove unused files, improve design
    remove unused files, improve design

--- partials/footer.php
+++ partials/footer.php
@@ -1,7 +1,6 @@
 <!-- ======= Footer ======= -->
 <footer id="footer" class="footer">
 	<div class="copyright">
-
 		<div class="credits">
             2021. IT Village. Code by Toshko & Kris.
 			Design by <a href="https://bootstrapmade.com/">BootstrapMade</a>
@@ -11,3 +10,13 @@
 	</div>
 </footer>
 <!-- ======= End Footer ======= -->
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="assets/vendor/aos/aos.js"></script>
+    <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+    <!-- Template Main JS File -->
+    <script src="assets/js/main.js"></script>
+</body>
+</html>
