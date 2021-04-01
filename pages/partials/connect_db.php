547b02df8dde53a8532b00bb7a7ac0aaa6e83ce3
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-04-01 17:48:14 +0300

    remove files from chris branch

--- pages/partials/connect_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$connection = mysqli_connect('127.0.0.1', 'root', '', 'itvillage');
-
-if (!$connection) {
-	die('Connection failed' . mysqli_connect_error() . ' - ' . mysqli_connect_errno());
-} else {
-	mysqli_set_charset($connection, "utf8");
-}
