33074d08c1362d598a7bf713af4bfae6fbe33900
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-04-04 19:23:54 +0300

    connect to db using Heroku (Production) credentials or logal ones
    connect to db using Heroku (Production) credentials or logal ones

--- pages/partials/connect_db.php
+++ pages/partials/connect_db.php
@@ -1,6 +1,12 @@
 <?php
 
-$connection = mysqli_connect('127.0.0.1', 'root', '', 'itvillage');
+
+$connection = mysqli_connect(
+	getenv('hostname') or '127.0.0.1',
+	getenv('username') or 'root',
+	getenv('password') or '',
+	getenv('database') or 'itvillage'
+);
 
 if (!$connection) {
 	die('Connection failed. ' . mysqli_connect_error() . ' - ' . mysqli_connect_errno());
