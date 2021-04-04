bee230c405f3514e2f7312135b2eadf0c796c1ee
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-04-04 19:52:42 +0300

    Update connect_db.php

--- pages/partials/connect_db.php
+++ pages/partials/connect_db.php
@@ -2,29 +2,28 @@
 
 if (getenv('environment') === 'production') {
 	$connection = mysqli_connect(
-		getenv('hostname'),
-		getenv('username'),
-		getenv('password'),
-		getenv('database')
+		getenv("hostname"),
+		getenv("username"),
+		getenv("password"),
+		getenv("database")
 	);
 }
 else {
 	$connection = mysqli_connect(
-		'127.0.0.1',
-		'root',
-		'',
-		'itvillage'
+		"127.0.0.1",
+		"root",
+		"",
+		"itvillage"
 	);
 }
 
 
 if (!$connection) {
-	echo "$connection";
-	echo getenv('hostname');
-	echo getenv('username');
-	echo getenv('password');
-	echo getenv('database');
-	die('Connection failed. ' . mysqli_connect_error() . ' - ' . mysqli_connect_errno(). );
+	echo getenv("hostname");
+	echo getenv("username");
+	echo getenv("password");
+	echo getenv("database");
+	die("Connection failed. " . mysqli_connect_error() . " - " . mysqli_connect_errno());
 } else {
 	mysqli_set_charset($connection, "utf8");
 }
