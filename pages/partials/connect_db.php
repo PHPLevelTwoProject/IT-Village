5f530a519b935d1b03f27e175a6f727438153449
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-04-05 19:55:08 +0300

    add composer and changes to connect db for heroku
    add composer and changes to connect db for heroku

--- pages/partials/connect_db.php
+++ pages/partials/connect_db.php
@@ -1,27 +1,35 @@
 <?php
 
-if (getenv('environment') == 'production') {
-	$hostname = getenv('hostname');
-	$username = getenv('username');
-	$password = getenv('password');
-	$database = getenv('database');
-	$CLEARDB_DATABASE_URL = getenv('CLEARDB_DATABASE_URL');
 
-	$connection = mysqli_connect(
-		$CLEARDB_DATABASE_URL
+//	$hostname = getenv('hostname');
+//	$username = getenv('username');
+//	$password = getenv('password');
+//	$database = getenv('database');
+//	$CLEARDB_DATABASE_URL = getenv('CLEARDB_DATABASE_URL');
+//
+//	$connection = mysqli_connect(
+//		$CLEARDB_DATABASE_URL
 //		$hostname,
 //		$username,
 //		$password,
 //		$database
-	);
+//	);
+
+if (getenv('environment') == 'production') {
+	//Get Heroku ClearDB connection information
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$active_group = 'default';
+	$query_builder = TRUE;
+
+	// Connect to DB
+	$connection = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 }
 else {
-	$connection = mysqli_connect(
-		"127.0.0.1",
-		"root",
-		"",
-		"itvillage"
-	);
+	$connection = mysqli_connect("127.0.0.1","root","","itvillage");
 }
 
 if (!$connection) {
