e027cdb16ff3d08219528e0e1ab9d1a9e0605a9f
shllshckd <52105864+shllshckd@users.noreply.github.com>
2021-04-13 23:36:04 +0300

    Revert "Revert "Revert "Revert "introduce extensive usage of functions for better separation of concerns""""
    This reverts commit 810e1b65d828600022bb8fa9e3022c75e97fde39.

--- pages/register.php
+++ pages/register.php
@@ -44,7 +44,13 @@ include './partials/header.php';
 </main>
 
 <?php
-if (isset($_POST['submit'])) {
+
+if (isset($_POST['submit']) && $connection) {
+	register($connection);
+}
+
+function register($connection)
+{
 	$flag = 'check ok';
 	if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) &&
 		preg_match('/^[a-zA-Z0-9]+$/', $_POST['password']) &&
@@ -52,39 +58,48 @@ if (isset($_POST['submit'])) {
 		$user = htmlspecialchars($_POST['username']);
 		$pass = htmlspecialchars($_POST['password']);
 		$repeat_pass = htmlspecialchars($_POST['repeat_password']);
+
 		if (getenv('environment') == 'production') {
 			$sql = "SELECT `username` FROM `heroku_6b647d0a28c075b`.`users` WHERE `username` = '$user' LIMIT 1";
 		} else {
 			$sql = "SELECT `username` FROM `itvillage`.`users` WHERE `username` = '$user' LIMIT 1";
 		}
-		$result = mysqli_query($connection, $sql);
+
+		if (isset($connection)) {
+			$result = mysqli_query($connection, $sql);
+		} else {
+			$result = null;
+		}
+
 		if (mysqli_num_rows($result) > 0) {
 			$flag = 'error name';
 		} else {
 			if ($pass != $repeat_pass) {
 				$flag = 'error pass';
-			}	
+			}
 		}
 	} else {
 		$flag = 'check error';
 	}
-	
+
 	if ($flag == 'check error') {
 		echo "<div class='text-center'><h1>Моля, въведете само букви и цифри.</h1></div>";
-	} elseif ($flag == 'error name'){
+	} elseif ($flag == 'error name') {
 		echo "<div class='text-center'><h1>Има потребител с това име.</h1></div>";
-	} elseif ($flag == 'error pass'){
-		echo "<div class='text-center'><h1>Моля, въведете еднаква парола.</h1></div>";	
+	} elseif ($flag == 'error pass') {
+		echo "<div class='text-center'><h1>Моля, въведете еднаква парола.</h1></div>";
 	} else {
-		$password_hashed = password_hash($pass,PASSWORD_DEFAULT);
+		$password_hashed = password_hash($pass, PASSWORD_DEFAULT);
 		$date = date("Y-m-d");
+
 		if (getenv('environment') == 'production') {
 			$add_sql = "INSERT INTO `heroku_6b647d0a28c075b`.`users`(`username`, `password`,`date_created` ) 
-                        VALUES ('$user', '$password_hashed', '$date')";
+                            VALUES ('$user', '$password_hashed', '$date')";
 		} else {
 			$add_sql = "INSERT INTO `itvillage`.`users`(`username`, `password`,`date_created` ) 
-                        VALUES ('$user', '$password_hashed', '$date')";
+                            VALUES ('$user', '$password_hashed', '$date')";
 		}
+
 		$add_result = mysqli_query($connection, $add_sql);
 		if ($add_result) {
 			echo "<div class='text-center'><h1>Успешна регистрация. <a href='login.php'>Вход</а>.</h1></div>";
