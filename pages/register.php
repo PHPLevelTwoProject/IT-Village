5a1872272652e0b3b938fb326465dafab7ec29e4
Toshko Todorov <toshko_montana@yahoo.com>
2021-04-20 21:20:04 +0300

    add characters

--- pages/register.php
+++ pages/register.php
@@ -20,19 +20,19 @@ include './partials/header.php';
                         <div class="form-group">
                             <label for="username">Потребителско име</label>
                             <input type="text" class="form-control" id="username" name="username">
-                            <small class="text-muted">Позволени са само букви и цифри</small>
+                            <small class="text-muted">Позволени са букви ( A-Z ,a-z ), цифри ( 0-9 ) и символи ( !@_#$%^*& )</small>
                         </div>
                         <br>
                         <div class="form-group">
                             <label for="password">Парола</label>
                             <input type="password" name="password" class="form-control" id="password">
-                            <small class="text-muted">Позволени са само букви и цифри</small>
+                            <small class="text-muted">Позволени са букви ( A-Z ,a-z ), цифри ( 0-9 ) и символи ( !@_#$%^*& )</small>
                         </div>
                         <br>
                         <div class="form-group">
                             <label for="repeat-password">Повторете паролата</label>
                             <input type="password" name="repeat_password" class="form-control" id="repeat-password">
-                            <small class="text-muted">Позволени са само букви и цифри</small>
+                            <small class="text-muted">Позволени са букви ( A-Z ,a-z ), цифри ( 0-9 ) и символи ( !@_#$%^*& )</small>
                         </div>
                         <br>
                         <input type="submit" name="submit" class="btn btn-outline-primary" value="Регистритай се">
@@ -44,65 +44,50 @@ include './partials/header.php';
 </main>
 
 <?php
-
-if (isset($_POST['submit']) && $connection) {
-	register($connection);
-}
-
-function register($connection)
-{
+if (isset($_POST['submit'])) {
 	$flag = 'check ok';
-	if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) &&
-		preg_match('/^[a-zA-Z0-9]+$/', $_POST['password']) &&
-		preg_match('/^[a-zA-Z0-9]+$/', $_POST['repeat_password'])) {
+	if (preg_match('/^[a-zA-Z0-9!@#$_%^&*]+$/', $_POST['username']) &&
+		preg_match('/^[a-zA-Z0-9!@#$_%^&*]+$/', $_POST['password']) &&
+		preg_match('/^[a-zA-Z0-9!@#$_%^&*]+$/', $_POST['repeat_password'])) {
 		$user = htmlspecialchars($_POST['username']);
 		$pass = htmlspecialchars($_POST['password']);
 		$repeat_pass = htmlspecialchars($_POST['repeat_password']);
-
 		if (getenv('environment') == 'production') {
 			$sql = "SELECT `username` FROM `heroku_6b647d0a28c075b`.`users` WHERE `username` = '$user' LIMIT 1";
 		} else {
 			$sql = "SELECT `username` FROM `itvillage`.`users` WHERE `username` = '$user' LIMIT 1";
 		}
-
-		if (isset($connection)) {
-			$result = mysqli_query($connection, $sql);
-		} else {
-			$result = null;
-		}
-
+		$result = mysqli_query($connection, $sql);
 		if (mysqli_num_rows($result) > 0) {
 			$flag = 'error name';
 		} else {
 			if ($pass != $repeat_pass) {
 				$flag = 'error pass';
-			}
+			}	
 		}
 	} else {
 		$flag = 'check error';
 	}
-
+	
 	if ($flag == 'check error') {
-		echo "<div class='text-center'><h1>Моля, въведете само букви и цифри.</h1></div>";
-	} elseif ($flag == 'error name') {
+		echo "<div class='text-center'><h1>Моля, въведете само букви ( A-Z , a-z ), цифри ( 0-9 ) и символи ( !@_#$%^*& ).</h1></div>";
+	} elseif ($flag == 'error name'){
 		echo "<div class='text-center'><h1>Има потребител с това име.</h1></div>";
-	} elseif ($flag == 'error pass') {
-		echo "<div class='text-center'><h1>Моля, въведете еднаква парола.</h1></div>";
+	} elseif ($flag == 'error pass'){
+		echo "<div class='text-center'><h1>Моля, въведете еднаква парола.</h1></div>";	
 	} else {
-		$password_hashed = password_hash($pass, PASSWORD_DEFAULT);
+		$password_hashed = password_hash($pass,PASSWORD_DEFAULT);
 		$date = date("Y-m-d");
-
 		if (getenv('environment') == 'production') {
 			$add_sql = "INSERT INTO `heroku_6b647d0a28c075b`.`users`(`username`, `password`,`date_created` ) 
-                            VALUES ('$user', '$password_hashed', '$date')";
+                        VALUES ('$user', '$password_hashed', '$date')";
 		} else {
 			$add_sql = "INSERT INTO `itvillage`.`users`(`username`, `password`,`date_created` ) 
-                            VALUES ('$user', '$password_hashed', '$date')";
+                        VALUES ('$user', '$password_hashed', '$date')";
 		}
-
 		$add_result = mysqli_query($connection, $add_sql);
 		if ($add_result) {
-			echo "<div class='text-center'><h1>Успешна регистрация. <a href='login.php'>Вход</а>.</h1></div>";
+			echo "<div class='text-center'><h1>Успешна регистрация. <a href='login.php'>Влез и играй</а>.</h1></div>";
 		} else {
 			echo "<div class='text-center'><h1>Неуспешна регистрация.</h1></div>";
 		}
